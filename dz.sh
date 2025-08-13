63646d0c96eb30fa7ca72d52a4ecef64fe778384
shlav11k <slavabalabanov739@gmail.com>
2025-08-13 19:56:31 +0300

    Dates of development of the improvement:Added time to the report %H:%M:%S

--- dz.sh
+++ dz.sh
@@ -4,26 +4,34 @@
 log_dir="logs"
 backup_dir="$HOME/backup"
 report_file="$backup_dir/report.txt"
+backup_file="backup-$(date +%Y-%m-%d).tar.gz"
 
 if [ ! -d "logs" ]; then 
-  echo "Error: Directory logs dose not exist!"
+  echo "Error: Directory logs dose not exist!" >&2
   exit 1 
 fi 
 
-mkdir -p "$backup_dir"
+mkdir -p "$backup_dir" || {
+  echo "Error: Faild to create backup directory '$backup_dir'" >&2 
+  exit 1
+}
 
-backup_file="backup-$(date +%Y-%m-%d).tar.gz"
-
+ 
 if ! find "$log_dir" -name '*.log' -type f -print0 | tar -czvf "$backup_dir/$backup_file" --null -T -; then 
   echo "Error: field create to backup arhive" >&2
   exit 1
 fi
 
 file_count=$(tar -tf "$backup_dir/$backup_file" | wc -l)
-archive_size=$(du -h "$backup_dir/$backup_file" | cut -f1)
+archive_size=$(du -h "$backup_dir/$backup_file" | awk '{print $1}')
 {
-echo "data: $(date)"
-echo "arhiv: $backup_file"
-echo "filov v arhive: $file_count"
-echo "rasmer: $archive_size"
-} > "$Report_file"
+echo "=== Backup Report ==="
+echo "Date: $(date + "%Y-%m-%d %H:%M:%S")"
+echo "Arhive: $backup_file" 
+echo "Files in archive: $file_count"
+echo "Size $archive_size"
+} > "$report_file" || {
+echo "Error: to create report_file" >&2
+exit 1
+}
+echo "Backup completed successfully. Report: $report_file"
