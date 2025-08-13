38a8df5cb71792dbc3b204eebae1e0e5f085ec32
shlav11k <slavabalabanov739@gmail.com>
2025-08-13 14:36:12 +0300

    an improved version has been added,the script now works and test-logs have been added

--- dz.sh
+++ dz.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-log_dir="$1"
+log_dir="logs"
 backup_dir="$HOME/backup"
 report_file="$backup_dir/report.txt"
 
-if[ ! -d "$test-logs" ]; then 
-  echo "Error: papka $test-logs proebalas!"
-  exit exit 1 
+if [ ! -d "logs" ]; then 
+  echo "Error: Directory logs dose not exist!"
+  exit 1 
 fi 
 
 mkdir -p "$backup_dir"
 
 backup_file="backup-$(date +%Y-%m-%d).tar.gz"
 
-find "$log_dir" -name '*.log' -type f -print0 | tar -czvf "$backup_dir/$backup_file" --null -T -
+if ! find "$log_dir" -name '*.log' -type f -print0 | tar -czvf "$backup_dir/$backup_file" --null -T -; then 
+  echo "Error: field create to backup arhive" >&2
+  exit 1
+fi
 
 file_count=$(tar -tf "$backup_dir/$backup_file" | wc -l)
 archive_size=$(du -h "$backup_dir/$backup_file" | cut -f1)
-
-echo "data: $(date)" > "$report_file"
-echo "arhiv: $backup_file" >> "$report_file"
-echo "filov v arhive: $file_count" >> "$report_file"
-echo "rasmer: $archive_size" >> "$report_file"
+{
+echo "data: $(date)"
+echo "arhiv: $backup_file"
+echo "filov v arhive: $file_count"
+echo "rasmer: $archive_size"
+} > "$Report_file"
